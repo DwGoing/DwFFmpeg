aaf0280e1b3b713b10061ba44741a008de0d0d7a
DwGoing <jianghy1209@163.com>
2021-01-05 13:57:00 +0800

    INIT：项目初始化

---
+++ src/DwFFmpeg.Runtime.MacOS/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DwFFmpeg.Runtime.MacOS
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/DwFFmpeg/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DwFFmpeg
+{
+    public class Class1
+    {
+    }
+}
